e602dd50fd81d41528b49382d352984023605fb3
archengdev <arc.albertcheng@gmail.com>
2024-08-07 20:22:21 +0800

    cleaned and finalized code

--- data_scraper.py
+++ data_scraper.py
@@ -2,7 +2,6 @@
 from csv import writer, DictReader
 
 import random
-import pandas as pd
 from sortedcontainers import SortedList
 import arrow
 import cassiopeia as cass
@@ -10,7 +9,7 @@ import cassiopeia as cass
 from private import API_KEY
 
 # get list of match id's that have already been looked at
-seen_match_ids = SortedList()
+seen_match_ids = set()
 with open('aram_damage_data.csv', 'r') as data:
     file = DictReader(data)
 
@@ -18,10 +17,10 @@ with open('aram_damage_data.csv', 'r') as data:
     for col in file:
         seen_match_ids.add(int(col['match_id']))
 
-# maybe move file outside? if opening and closing takes a while
+
 def add_to_csv(match, writer_obj):
     """
-    takes a cass match object and adds relevant data to csv
+    takes a cass match object, and writer object, and adds relevant data to csv
     adds match id, and:
     champ name, magic damage dealt, physical damage dealt, damage mitigated
     for each of the 10 players
@@ -34,8 +33,9 @@ def add_to_csv(match, writer_obj):
         lst.append(stats.physical_damage_dealt_to_champions)
         lst.append(stats.damage_self_mitigated)
     writer_obj.writerow(lst)
+    # print("done")
 
-# set the api key
+# set the api key and turn off printing API calls
 cass.set_riot_api_key(API_KEY)
 cass.print_calls(False)
 
@@ -44,13 +44,13 @@ account = cass.get_account(name="SirFlash", tagline="NA1", region="NA")
 summoner = account.summoner
 
 # choose a patch to analyse games for
-patch = cass.Patch.from_str("9.19", region="NA")
+patch = cass.Patch.from_str("14.15", region="NA")
 
 # create a sorted list for player ID's (starting from the initial summoner) 
 unseen_puuids = SortedList([summoner.puuid])
-seen_puuis = SortedList()
+seen_puuis = set()
 
-# create a sorted list for ARAM match ID's to crawl through
+# create a list of ARAM match ID's to crawl through
 unseen_matches = []
 
 ctnt = summoner.region.continent
@@ -79,7 +79,6 @@ with open('aram_damage_data.csv', 'a', newline='') as file:
         # update puuid lists
         unseen_puuids.remove(new_puuid)
         seen_puuis.add(new_puuid)
-        print(unseen_puuids)
 
         # loop over all unseen matches, and add info to csv
         while unseen_matches and len(seen_match_ids) < num_matches:
@@ -107,29 +106,3 @@ with open('aram_damage_data.csv', 'a', newline='') as file:
                 print(len(seen_match_ids))
 
     file.close()
-# filter_match_history(summoner.puuid, patch)
-# match_history = cass.get_match_history(puuid=summoner.puuid, start_time = patch.start, end_time= arrow.now(), queue=cass.Queue.aram, continent=ctnt )
-# with open('aram_damage_data.csv', 'a', newline='') as file:
-        
-#         for match in unseen_matches:
-#             add_to_csv(match, writer_obj)
-#             print("done")
-#         file.close()
-# for match in unseen_matches:
-#     currID = match.id
-#     try:
-#         for p in match.participants:
-#             print(p.summoner.puuid)
-#     except:
-#         print(currID, "failed")
-quit()
-
-match = random.choice(unseen_matches)
-S1_stats = match.participants[0].stats
-print(match.id, match.participants[0].champion.name, S1_stats.magic_damage_dealt_to_champions, S1_stats.physical_damage_dealt_to_champions, S1_stats.damage_self_mitigated)
-print(match.participants[1].summoner.puuid)
-# for i in match_history:
-#     print(i.id)
-# print("matchID:", match.id)
-# new_match = cass.Match(id=match, region="NA")
-# print(new_match)
