66c8e724f46fe702174ff7afc18fcf6a177289e6
archengdev <arc.albertcheng@gmail.com>
2024-08-22 21:22:45 +0800

    implemented random forest

--- random_forest.py
+++ random_forest.py
@@ -1,34 +1,36 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
-# read raw data from csv
-data = pd.read_csv("./9.19/analysis_test.csv")
-# print(data)
+from test_train_data import get_data
 
-# create dataframe for data correlated to winning team
-X_win = (data[['winning_mag', 'winning_phy', 'winning_mit', 'winning_magphy', 'winning_phymit', 'winning_magphymit']]
-         .rename(columns={'winning_mag':'mag', 'winning_phy':'phy', 'winning_mit':'mit', 'winning_magphy':'magphy', 'winning_phymit':'phymit', 'winning_magphymit':'magphymit'}))
-# print(X_win)
 
-# create y variable for winning team (all 1's)
-oneGen = ((1 for x in range(X_win.shape[0])))
-Y_win = pd.DataFrame(oneGen, columns=['Win'])
-# print(Y_win)
+# split data into training and test set
+X_train, X_test, y_train, y_test = get_data("./9.19/processed_data.csv")
+y_train = y_train.values.flatten()
+y_test = y_test.values.flatten()
 
-# create dataframe for data correlated to losing team
-X_loss = (data[['losing_mag', 'losing_phy', 'losing_mit', 'losing_magphy', 'losing_phymit', 'losing_magphymit']]
-          .rename(columns={'losing_mag':'mag', 'losing_phy':'phy', 'losing_mit':'mit', 'losing_magphy':'magphy', 'losing_phymit':'phymit', 'losing_magphymit':'magphymit'}))
-# print(X_loss)
+# do random forest
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=3)
 
-# create y variable for losing team (all 0's)
-zeroGen = ((0 for x in range(X_loss.shape[0])))
-Y_loss = pd.DataFrame(zeroGen, columns=['Win'])
-# print(Y_loss)
+rf_classifier.fit(X_train, y_train)
 
-# concatenate wins and losses for analysis
-Y = pd.concat([Y_win, Y_loss], ignore_index=True)
-X = pd.concat([X_win, X_loss], ignore_index=True)
-# print(X)
-# sklearn.tes
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=3)
-print(X_train)
+y_pred = rf_classifier.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+classification_rep = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy:.2f}")
+print("Classification Report:\n", classification_rep)
+
+"""
+Accuracy: 0.49
+Classification Report:
+               precision    recall  f1-score   support
+
+           0       0.49      0.51      0.50      2874
+           1       0.49      0.48      0.48      2884
+
+    accuracy                           0.49      5758
+   macro avg       0.49      0.49      0.49      5758
+weighted avg       0.49      0.49      0.49      5758
+"""
